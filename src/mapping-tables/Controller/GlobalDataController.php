3d162c9b3e9afc68d271b83ac1c32c039bc372fc
Christoph Klöppner <christoph.kloeppner@mza.de>
2025-07-02 17:57:23 +0200

    - renamed customer group

--- src/mapping-tables/Controller/GlobalDataController.php
+++ src/mapping-tables/Controller/GlobalDataController.php
@@ -57,7 +57,7 @@ class GlobalDataController implements PullInterface, PushInterface
             (new CustomerGroup())->setId(new Identity(AbstractController::CUSTOMER_TYPE_B2B))
                 ->setIsDefault(false)
                 ->setApplyNetPrice(true)
-                ->addI18n((new CustomerGroupI18n())->setName('Händler')->setLanguageIso('ger'))
+                ->addI18n((new CustomerGroupI18n())->setName('MZA B2B')->setLanguageIso('ger'))
         );
 
         // TaxRates
